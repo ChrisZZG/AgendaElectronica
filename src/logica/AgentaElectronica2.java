68706f77747f73a67ad3c8bfdbcff19d112362f2
ChrisZZG <cristiangonzz@gmail.com>
2023-08-05 19:07:15 -0300

    Primer commit de la practica terminada

---
+++ src/logica/AgentaElectronica2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author EQUIPO
+ */
+public class AgentaElectronica2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+    
+}
